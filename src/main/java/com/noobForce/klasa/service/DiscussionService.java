02fbf52517ac4d66c9d53caa191617a0401cadf3
Ishita <ishitab2001@gmail.com>
2021-06-21 18:47:16 +0530

    discussion controller, service, repository updated

--- src/main/java/com/noobForce/klasa/service/DiscussionService.java
+++ src/main/java/com/noobForce/klasa/service/DiscussionService.java
@@ -1,4 +1,34 @@
 package com.noobForce.klasa.service;
 
+import com.noobForce.klasa.model.Discussion;
+import com.noobForce.klasa.repository.DiscussionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class DiscussionService {
+
+    @Autowired
+    DiscussionRepository discussionRepository;
+
+    private static final Logger log = LoggerFactory.getLogger(DiscussionService.class);
+
+    public List<Discussion> getAllDiscussion() {
+        return discussionRepository.findAll();
+    }
+
+    public List<Discussion> getByDate(int date)
+    {
+        return discussionRepository.findByDate(LocalDate.of(2021, 6,date));
+    }
+
+    public Discussion addDiscussion(Discussion discussion) {
+        log.info("I'm here too");
+        return discussionRepository.save(discussion);
+    }
 }
